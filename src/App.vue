e7923a079f1ae695a552e3f5ca628dde7d443a6f
matheuspython <ruivorobado@gmail.com>
2022-07-28 18:05:42 -0300

    app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <TheHeader />
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+}
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: sans-serif, Helvetica, Arial;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    header
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'TheHeader',
+
+  }
+</script>
+
+<style scoped>
+  header{
+    width: 100%;
+    height: 40px;
+    background-color: #6329bb;
+    text-align: center;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
